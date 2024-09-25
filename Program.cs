ee1673af5fbff02a853d0e3cb770ada19633fdbc
assistant1277 <shivarajghangale2002@gmail.com>
2024-09-25 14:32:34 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using PalindromeNumber.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number to check if it is palindrome ");
+            int inputNumber = int.Parse(Console.ReadLine());
+            Palindrome.CheckAndPrintPalindrome(inputNumber);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ model/Palindrome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumber.model
+{
+    public class Palindrome
+    {
+        public static void CheckAndPrintPalindrome(int number)
+        {
+            int originalNumber = number;
+            int reversedNumber = 0;
+
+            while (number > 0)
+            {
+                int lastDigit = number % 10;
+                reversedNumber = (reversedNumber * 10) + lastDigit;
+                number /= 10;
+            }
+            string resultMessage = (originalNumber == reversedNumber) ? $"{originalNumber} is palindrome number" : $"{originalNumber} not a palindrome number";
+            Console.WriteLine(resultMessage);
+        }
+    }
+}
